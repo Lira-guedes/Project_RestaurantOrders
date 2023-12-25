3e61418324598e10901c9b94eb0c9045a32fe231
Lira Guedes <liracrg@gmail.com>
2023-12-25 14:52:18 -0300

    test: req 2 Ingredients

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,20 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
-
+from src.models.ingredient import Ingredient, Restriction 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("queijo mussarela")
+    ingredient3 = Ingredient("bacon")
+
+    assert ingredient == ingredient2
+    assert ingredient != ingredient3
+    assert hash(ingredient) == hash(ingredient2)
+    assert hash(ingredient) != hash(ingredient3)
+
+    assert repr(ingredient) == "Ingredient('queijo mussarela')"
+    assert ingredient.name == "queijo mussarela"
+
+    assert ingredient.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED
+    }
