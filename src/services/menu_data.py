0437e78891850304ea6f9e517e6d4b7942c83fc2
Lira Guedes <liracrg@gmail.com>
2023-12-25 17:07:45 -0300

    feat: req 3 Menu Data

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,34 @@
+from models.ingredient import Ingredient
+from models.dish import Dish
+import csv
+
+
 # Req 3
 class MenuData:
-    def __init__(self, source_path: str) -> None:
-        pass
+    def __init__(self, source_path) -> None:
+        self.dishes = set()
+        self._menu_data(source_path)
+
+    def _menu_data(self, source_path) -> None:
+        with open(source_path, newline='') as csvfile:
+            menu = csv.DictReader(csvfile)
+
+            for row in menu:
+                name = row['dish']
+                price = float(row['price'])
+
+                # Verifica se um prato com o mesmo nome e preço já existe
+                dish = next((
+                    dis for dis in self.dishes
+                    if dis.name == name and dis.price == price
+                ), None)
+
+                # Se o prato não existir, cria um novo e adiciona ao conjunto
+                if not dish:
+                    dish = Dish(name, price)
+                    self.dishes.add(dish)
+
+                dish.add_ingredient_dependency(
+                    Ingredient(row['ingredient']),
+                    int(row['recipe_amount'])
+                )
